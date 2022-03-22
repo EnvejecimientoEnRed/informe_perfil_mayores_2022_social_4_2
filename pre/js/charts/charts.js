441c18e30ad8beafca89a00f3001163f72c4c5dc
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-03-22 09:27:27 +0100

    Cambio de datos + Continuación desarrollo

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -21,10 +21,70 @@ COLOR_OTHER_2 = '#731854';
 
 export function initChart(iframe) {
     //Desarrollo del gráfico
-    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_social_4_2/main/data/estado_civil_65_1970_2020.csv', function(error,data) {
+    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_social_4_2/main/data/estado_civil_65_1970_2020_csv.csv', function(error,data) {
         if (error) throw error;
 
         console.log(data);
+
+        let margin = {top: 10, right: 30, bottom: 20, left: 50},
+            width = document.getElementById('chart').clientWidth - margin.left - margin.right,
+            height = document.getElementById('chart').clientHeight - margin.top - margin.bottom;
+
+        let svg = d3.select("#chart")
+        .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        let estados = data.columns.slice(1)
+        let periodos = d3.map(data, function(d){return(d.Periodo)}).keys();
+
+        let x = d3.scaleBand()
+            .domain(periodos)
+            .range([0, width])
+            .padding([0.2]);
+        
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x).tickSize(0));
+
+        let y = d3.scaleLinear()
+            .domain([0, 100])
+            .range([ height, 0 ]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        let xSubgroup = d3.scaleBand()
+            .domain(estados)
+            .range([0, x.bandwidth()])
+            .padding([0.05]);
+        
+        let color = d3.scaleOrdinal()
+            .domain(estados)
+            .range([COLOR_PRIMARY_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_OTHER_1]);
+
+        function init() {
+            svg.append("g")
+                .selectAll("g")
+                .data(data)
+                .enter()
+                .append("g")
+                .attr("transform", function(d) { return "translate(" + x(d.Periodo) + ",0)"; })
+                .selectAll("rect")
+                .data(function(d) { return estados.map(function(key) { return {key: key, value: d[key]}; }); })
+                .enter().append("rect")
+                .attr("x", function(d) { return xSubgroup(d.key); })
+                .attr("y", function(d) { return y(d.value); })
+                .attr("width", xSubgroup.bandwidth())
+                .attr("height", function(d) { return height - y(d.value); })
+                .attr("fill", function(d) { return color(d.key); });
+
+        }
+
+        function animateChart() {
+
+        }
 
         /////
         /////
